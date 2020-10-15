c06463aae3e2269c72f2d92ada18335d0949df7d
TristanDiabolo <tristan@diabolo-web.com>
2020-10-15 11:10:18 +0200

    Modify user profile and modify password #16

--- src/Controller/Manage/ManageUserController.php
+++ src/Controller/Manage/ManageUserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller\Manage;
 
-use App\Form\RegistrationFormType;
+use App\Form\PasswordFormType;
+use App\Form\UserFormType;
 use App\Repository\UserRepository;
 use App\Security\LoginFormAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,14 +20,32 @@ class ManageUserController extends AbstractController
      * @var UserRepository
      */
     private $userRepository;
+    /**
+     * @var GuardAuthenticatorHandler
+     */
+    private $guardHandler;
+    /**
+     * @var LoginFormAuthenticator
+     */
+    private $authenticator;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
 
     /**
      * ManageUserController constructor.
+     * @param EntityManagerInterface $entityManager
      * @param UserRepository $userRepository
+     * @param GuardAuthenticatorHandler $guardHandler
+     * @param LoginFormAuthenticator $authenticator
      */
-    public function __construct(UserRepository $userRepository)
+    public function __construct(EntityManagerInterface $entityManager, UserRepository $userRepository, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator)
     {
         $this->userRepository = $userRepository;
+        $this->guardHandler = $guardHandler;
+        $this->authenticator = $authenticator;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -33,57 +53,107 @@ class ManageUserController extends AbstractController
      * @param int $id
      * @param string $slug
      * @param Request $request
-     * @param UserPasswordEncoderInterface $passwordEncoder
-     * @param GuardAuthenticatorHandler $guardHandler
-     * @param LoginFormAuthenticator $authenticator
      * @return Response
      */
-    public function update(int $id,
-                           string $slug,
-                           Request $request,
-                           UserPasswordEncoderInterface $passwordEncoder,
-                           GuardAuthenticatorHandler $guardHandler,
-                           LoginFormAuthenticator $authenticator
-    )
+    public function update(int $id, string $slug, Request $request)
     {
         $user = $this->userRepository->find($id);
         $userSlug = $user->getSlug();
 
         if ($userSlug !== $slug) {
-            return $this->redirectToRoute("user.show", [
+            return $this->redirectToRoute("manage.user.update", [
                 "id" => $user->getId(),
                 "slug" => $userSlug
             ], 301);
         }
 
-        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form = $this->createForm(UserFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
-            $user->setPassword(
-                $passwordEncoder->encodePassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $guardHandler->authenticateUserAndHandleSuccess(
+            $this->addFlash("success", "Votre profil a bien été modifié");
+            return $this->guardHandler->authenticateUserAndHandleSuccess(
                 $user,
                 $request,
-                $authenticator,
+                $this->authenticator,
                 'main' // firewall name in security.yaml
             );
         }
 
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
-            'current_menu' => 'app_register'
+        return $this->render('manage/user/form.html.twig', [
+            "user" => $user,
+            "userForm" => $form->createView(),
+            "current_menu" => "app_user_modify"
+        ]);
+    }
+
+    /**
+     * @Route("/manage/user/password/{id}-{slug}", name="manage.password.update", requirements={"slug": "[a-z0-9\-]*", "id": "[0-9]*"})
+     * @param int $id
+     * @param string $slug
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return Response
+     */
+    public function passwordUpdate(int $id, string $slug, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = $this->userRepository->find($id);
+        $userSlug = $user->getSlug();
+
+        if ($userSlug !== $slug) {
+            return $this->redirectToRoute("manage.password.update", [
+                "id" => $user->getId(),
+                "slug" => $userSlug
+            ], 301);
+        }
+
+        $form = $this->createForm(PasswordFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $oldPwd = $form->get("old_password")->getData();
+            $newPwd = $form->get("new_password")->getData();
+            $newPwdConfirm = $form->get("new_password_confirm")->getData();
+
+            if ($passwordEncoder->isPasswordValid($user, $oldPwd) && $newPwd == $newPwdConfirm) {
+                $user->setPassword(
+                    $passwordEncoder->encodePassword(
+                        $user,
+                        $newPwd
+                    )
+                );
+
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+                $this->addFlash("success", "Le mot de passe a bien été modifié");
+//                return $this->redirectToRoute("user.show");
+            }
+            return $this->guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $this->authenticator,
+                'main'
+            );
+
+            if (!$passwordEncoder->isPasswordValid($user, $oldPwd)) {
+                $this->addFlash("error", "L'ancien mot de passe n'est pas valide");
+            }
+
+            if ($newPwd != $newPwdConfirm) {
+                $this->addFlash("error", "Les mot de passe ne sont pas identiques");
+            }
+
+            return $this->redirectToRoute("manage.password.update", ["id" => $user->getId(), "slug" => $userSlug]);
+        }
+
+        return $this->render('manage/user/password-form.html.twig', [
+            'passwordForm' => $form->createView(),
+            'current_menu' => 'app_user_modify'
         ]);
     }
 }
